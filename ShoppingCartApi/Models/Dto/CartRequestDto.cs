e519f9fb2146810cdbc13fc243baaef6359c52e5
jeetpatel313 <jp2398d@gmail.com>
2024-06-17 14:39:15 -0700

    Updated name and added comments

---
+++ ShoppingCartApi/Models/Dto/CartRequestDto.cs
@@ -0,0 +1,11 @@
+namespace ShoppingCartApi.Models.Dto.Carts
+{
+  public class CartRequestDto
+  {
+    // The unique identifier for the product to be added in the cart.
+    public int ProductId { get; set; }
+
+    // The quantity of the product to be added in the cart.
+    public int Quantity { get; set; }
+  }
+}
